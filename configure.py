4b005a23b79af4b542c4881154d3fff223717e0b
Jovinull <felipejovinogamerplay@gmail.com>
2026-07-19 20:13:10 -0300

    chore: drop unneeded common off flag

--- configure.py
+++ configure.py
@@ -305,7 +305,7 @@ config.libs = [
             Object(Matching, "dolphin/os/__start.c"),
             Object(Matching, "dolphin/os/OSArena.c"),
             Object(Matching, "dolphin/base/PPCArch.c"),
-            Object(Matching, "dolphin/db/db.c", extra_cflags=["-common off"]),
+            Object(Matching, "dolphin/db/db.c"),
             Object(Matching, "dolphin/os/OSCache.c"),
             Object(Matching, "dolphin/os/OSTime.c"),
             Object(Matching, "Runtime.PPCEABI.H/__ppc_eabi_init.c"),
