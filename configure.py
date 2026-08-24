0c3aad0db8df5aaa514c8869d839929478f9d79c
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-23 21:36:32 -0500

    decomp: match MSL w_log10 (#278)
    * decomp: match MSL e_acos
    
    * decomp: match MSL e_asin
    
    * decomp: match MSL e_atan2
    
    * decomp: match MSL e_fmod
    
    * decomp: match MSL e_log
    
    * decomp: match MSL e_log10
    
    * decomp: match MSL e_pow
    
    * decomp: match MSL e_rem_pio2
    
    * decomp: match MSL k_cos
    
    * decomp: match MSL k_rem_pio2
    
    * decomp: match MSL k_sin
    
    * decomp: match MSL k_tan
    
    * decomp: match MSL s_atan
    
    * decomp: match MSL s_ceil
    
    * decomp: match MSL s_copysign
    
    * decomp: match MSL s_cos
    
    * decomp: match MSL s_floor
    
    * decomp: match MSL s_frexp
    
    * decomp: match MSL s_ldexp
    
    * decomp: match MSL modf
    
    * decomp: match MSL s_nextafter
    
    * decomp: name modf translation unit correctly
    
    * decomp: match MSL s_sin
    
    * decomp: match MSL s_tan
    
    * decomp: match MSL w_acos
    
    * decomp: match MSL w_asin
    
    * decomp: match MSL w_atan2
    
    * decomp: match MSL w_fmod
    
    * decomp: match MSL w_log
    
    * decomp: match MSL w_log10
    
    ---------
    
    Co-authored-by: Jovinull <felipejovinogamerplay@gmail.com>

--- configure.py
+++ configure.py
@@ -356,6 +356,8 @@ config.libs = [
             Object(Matching, "MSL_C/w_asin.c"),
             Object(Matching, "MSL_C/w_atan2.c"),
             Object(Matching, "MSL_C/w_fmod.c"),
+            Object(Matching, "MSL_C/w_log.c"),
+            Object(Matching, "MSL_C/w_log10.c"),
         ],
     },
     {
