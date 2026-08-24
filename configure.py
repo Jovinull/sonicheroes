9e408d55dd0a693016a4bbefaa08827a4132262d
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-23 20:40:07 -0500

    decomp: match MSL k_cos

--- configure.py
+++ configure.py
@@ -336,6 +336,7 @@ config.libs = [
             Object(Matching, "MSL_C/e_log10.c"),
             Object(Matching, "MSL_C/e_pow.c"),
             Object(Matching, "MSL_C/e_rem_pio2.c"),
+            Object(Matching, "MSL_C/k_cos.c"),
         ],
     },
     {
