6a9961c369cb28378a82cc576abdac074174e42c
Felipe Jovino <79649732+Jovinull@users.noreply.github.com>
2026-08-23 22:26:47 -0300

    decomp: match MSL e_acos and e_asin (integration of #242) (#246)
    * decomp: match MSL e_acos
    
    * decomp: match MSL e_asin
    
    ---------
    
    Co-authored-by: Cypress <140924725+cypressru@users.noreply.github.com>

---
+++ include/MSL_C/math.h
@@ -0,0 +1,32 @@
+#ifndef MSL_C_MATH_H
+#define MSL_C_MATH_H
+
+#include "types.h"
+
+extern s32 lbl_8042BEE8[];
+extern s32 lbl_8042BEEC[];
+
+#define NAN       (*(float*)lbl_8042BEE8)
+#define HUGE_VALF (*(float*)lbl_8042BEEC)
+
+double __frsqrte(double x);
+double __fabs(double x);
+
+extern inline double sqrt(double x)
+{
+	if (x > 0.0) {
+		double guess = __frsqrte(x);
+		guess        = 0.5 * guess * (3.0 - guess * guess * x);
+		guess        = 0.5 * guess * (3.0 - guess * guess * x);
+		guess        = 0.5 * guess * (3.0 - guess * guess * x);
+		guess        = 0.5 * guess * (3.0 - guess * guess * x);
+		return x * guess;
+	}
+	if (x == 0.0)
+		return 0.0;
+	if (x)
+		return NAN;
+	return HUGE_VALF;
+}
+
+#endif
