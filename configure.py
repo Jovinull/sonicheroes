5a0bce3b419e7eb76645b58ed657f2c235cb5426
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-25 18:40:28 -0500

    Decompile CRI SVM translation unit (#383)
    * Decompile CRI SVM translation unit
    
    * Fix CRI SJ split registration order
    
    * wip: land the SVM bss at the target offsets with a layout anchor
    
    * decomp: land the SVM source without the object post-processor
    
    ---------
    
    Co-authored-by: Jovinull <felipejovinogamerplay@gmail.com>

--- configure.py
+++ configure.py
@@ -582,6 +582,11 @@ config.libs = [
             ),
             Object(
                 NonMatching,
+                "game/cri/svm.c",
+                extra_cflags=["-sdata 0", "-sdata2 0", "-str reuse,readonly", "-use_lmw_stmw on"],
+            ),
+            Object(
+                NonMatching,
                 "game/cri/axrna.c",
                 extra_cflags=["-sdata 0", "-sdata2 0", "-str reuse,readonly", "-use_lmw_stmw on"],
             ),
