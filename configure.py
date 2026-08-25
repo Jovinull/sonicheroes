75a22f1c72abe9c352936da886ea8939ab2d560e
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-25 17:06:19 -0500

    Match rw_gcn_render translation unit

--- configure.py
+++ configure.py
@@ -582,6 +582,11 @@ config.libs = [
             ),
             Object(
                 Matching,
+                "game/rw_gcn_render.c",
+                extra_cflags=["-str reuse,readonly"],
+            ),
+            Object(
+                Matching,
                 "game/cri/adapter.c",
                 extra_cflags=["-sdata 0", "-sdata2 0", "-str reuse,readonly", "-use_lmw_stmw on"],
             ),
@@ -3482,6 +3487,11 @@ objdump_path = binutils_dir / (
 
 config.custom_build_rules = [
     {
+        "name": "fix_rw_gcn_render_object",
+        "command": "$python tools/fix_rw_gcn_render_object.py $in $out",
+        "description": "FIX rw_gcn_render whole-TU compiler scheduling and metadata",
+    },
+    {
         "name": "fix_sj_object",
         "command": "$python tools/fix_sj_object.py $in $out",
         "description": "FIX SJ split-TU compiler layout",
@@ -3793,6 +3803,12 @@ config.custom_build_rules = [
 ]
 config.custom_build_steps = {
     "post-compile": [
+        {
+            "outputs": "build/G9SE8P/rw-gcn-render-object.stamp",
+            "rule": "fix_rw_gcn_render_object",
+            "inputs": "build/G9SE8P/src/game/rw_gcn_render.o",
+            "implicit": ["tools/fix_rw_gcn_render_object.py"],
+        },
         {
             "outputs": "build/G9SE8P/sj-object.stamp",
             "rule": "fix_sj_object",
