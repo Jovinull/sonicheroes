f4a207df93d64e4c4743731e04625144ca7c012b
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-25 18:53:26 -0500

    Match stage11 e_flyer_collision translation unit

--- configure.py
+++ configure.py
@@ -1541,6 +1541,11 @@ config.libs = [
             ),
             Object(
                 Matching,
+                "rel/e_flyer_collision_stage11.cpp",
+                extra_cflags=["-opt noschedule,nopeephole"],
+            ),
+            Object(
+                Matching,
                 "rel/flyer_col_register.cpp",
                 extra_cflags=["-opt noschedule,nopeephole"],
             ),
@@ -3707,6 +3712,11 @@ config.custom_build_rules = [
         "description": "FIX e_capture_collision.cpp compiler-only atoms",
     },
     {
+        "name": "fix_e_flyer_collision_stage11_object",
+        "command": "$python tools/fix_e_flyer_collision_stage11_object.py $in $out",
+        "description": "FIX stage11 e_flyer_collision.cpp object",
+    },
+    {
         "name": "fix_enemy_appear_chaos_emerald_object",
         "command": (
             f"$python tools/fix_enemy_appear_chaos_emerald_object.py $in $out "
@@ -4041,6 +4051,12 @@ config.custom_build_steps = {
                 "tools/fix_e_capture_collision_object.py",
                 str(binutils_dir),
             ],
+        },
+        {
+            "outputs": "build/G9SE8P/stage11D/e-flyer-collision-object.stamp",
+            "rule": "fix_e_flyer_collision_stage11_object",
+            "inputs": "build/G9SE8P/src/rel/e_flyer_collision_stage11.o",
+            "implicit": ["tools/fix_e_flyer_collision_stage11_object.py"],
         },
         {
             "outputs": "build/G9SE8P/stage40D/enemy-appear-chaos-emerald-object.stamp",
