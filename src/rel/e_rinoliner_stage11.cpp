3486623ea7ec32ddbbc18eedc2d31e9447ffda3e
Cypress <140924725+cypressru@users.noreply.github.com>
2026-08-26 03:35:26 -0500

    Complete the stage 11 rinoliner translation unit

--- src/rel/e_rinoliner_stage11.cpp
+++ src/rel/e_rinoliner_stage11.cpp
@@ -130,6 +130,8 @@ typedef struct TObject {
 	/* 0x31C */ s32 unk31C;              /* inferred */
 } TObject;                               /* size >= 0x320 */
 
+extern "C" {
+
 M2C_UNK SetPosAng__15TEnemyParalysisFPC5RwV3dPC6sAngle(...);               /* extern */
 M2C_UNK Vibrate__15TEnemyParalysisFP7RwFrame15RwOpCombineType(...);        /* extern */
 void* __ct__10HAnimClassFv(...);                                           /* extern */
@@ -1305,3 +1307,4 @@ void rinolinerObjectRegister(void)
 }
 
 __declspec(section ".ctors") void (*const rinolinerObjectCtorEntry)(void) = rinolinerObjectRegister;
+}
